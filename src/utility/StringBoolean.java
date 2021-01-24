c81b9cecf690319975b63ba231cf288a35f14dd7
naaem <naaem@gmail.com>
2021-01-24 16:42:13 +0530

    string

--- src/utility/StringBoolean.java
+++ src/utility/StringBoolean.java
@@ -1,9 +1,36 @@
 package utility;
+/*
+1.*Go to StringBoolean class and create the following methods.*
+2.*Write a method to check if the string1 contains string2.*
+3.*Write a method to to check if the string starts with the given letter.*
+4.*Write a method to to check if the string end with the given letter.*
+5.*Write a method to to check if two strings are equal.*
+6.*Write a method to to check if two strings are equal by ignoring the case. for example prograd && PROgrAD should return true.* 
+7.*All the methods should return the value as boolean.*
 
+*Go to StringBoolean class and create the following methods.*
+2.*Write a method to concatenate two strings.*
+3.*Write a method to split the first string based on the second string value.*
+4.*Write a method to find the index value of the given character in the first string.* 
+5.*All the methods should return the value as String.*
+
+*/
 public class StringBoolean {
 	private String input1;
 	private String input2;
-
+     public String concatenate(String s1,String s2) {
+    	 return s1+s2;
+     }
+     
+     public int indexOf(String s1,String s2) {
+    	 return s1.indexOf(s2);
+     }
+     
+     public String[] split(String s1,String s2) {
+    	 String[] strings = s1.split(s2);
+    	 return strings;
+     }
+     
 	public String getInput1() {
 		return input1;
 	}
@@ -11,7 +38,7 @@ public class StringBoolean {
 	public void setInput1(String input1) {
 		this.input1 = input1;
 	}
-
+     
 	public String getInput2() {
 		return input2;
 	}
--- src/utility/StringMethod.java
+++ src/utility/StringMethod.java
@@ -1,3 +1,33 @@
 package utility;
 
 // Type your code
+/**Go to StringMethod class and create the following methods.*
+2.*Write a method to convert the String to lowercase.*
+3.*Write a method to convert the String to uppercase.*
+4.*Write a method to find the first character in the string.*
+5.*Write a method to return string length.*
+6.*Write a method to trim the input string.* 
+7.*All the methods should return the value as string.*
+*/
+public class StringMethod{
+	String string = "";
+	public String toLowerCase(String str) {
+		return str.toLowerCase();
+	}
+	public String toUpperCase(String str) {
+	    return str.toUpperCase();	
+	}
+	public String firstChar(String str) {
+		return str.charAt(0)+" ";
+	}
+	public int stringLength(String str) {
+		return str.length();
+	}
+	public String trim(String str) {
+		return str.trim();
+	}
+	public void setInput(String input) {
+		// TODO Auto-generated method stub
+		string = input;
+	}
+}
